40b98296df9c99091368980232ce210abc3675bd
Summerdayxmm <32258654+Summerdayxmm@users.noreply.github.com>
2023-08-28 08:52:54 +0800

    add LeetCode370

---
+++ LabuLadong/src/main/java/com/enchanted/Array/LeetCode/LeetCode370.java
@@ -0,0 +1,56 @@
+package com.enchanted.Array.LeetCode;
+
+/**
+ * @ClassName: LeetCode370
+ * @Description: 区间加法
+ * @PackageName:com.enchanted.Array.LeetCode
+ * @Author Enchanted
+ * @Date 2023/8/28 07:10
+ * @Version 1.0
+ */
+ public class LeetCode370 {
+     public int[] getModifiedArray(int length, int[][] updates) {
+         int[] nums = new int[length];
+         Difference df = new Difference(nums);
+         for(int[] update: updates) {
+             int i = update[0];
+             int j = update[1];
+             int val = update[2];
+             df.increment(i, j, val);
+         }
+
+         return df.result();
+
+     }
+ }
+
+ class Difference {
+     private int[] diff;
+
+     public Difference(int[] nums) {
+         assert nums.length > 0;
+         diff = new int[nums.length];
+         diff[0] = nums[0];
+         for (int i = 1; i < nums.length; i++) {
+             diff[i] = nums[i] - nums[i - 1];
+         }
+     }
+
+     public void increment(int i, int j, int val) {
+         diff[i] += val;
+         if(j + 1 < diff.length) {
+             diff[j + 1] -= val;
+         }
+     }
+
+     public int[] result() {
+         int[] res = new int[diff.length];
+         res[0] = diff[0];
+         for (int i = 1; i < diff.length; i++) {
+             res[i] = res[i - 1] + diff[i];
+         }
+         return res;
+     }
+ }
+
+
--- LeetCode/src/main/java/com/enchanted/LRUCache.java
+++ LeetCode/src/main/java/com/enchanted/LRUCache.java
@@ -1,86 +1,86 @@
-package com.enchanted;
-
-
-/**
- * @ClassName: LRUCache
- * @Description: LeetCode146.LRU缓存
- * @PackageName:com.enchanted
- * @Author Enchanted
- * @Date 2023/8/18 20:38
- * @Version 1.0
- */
-
-
-public class LRUCache {
-
-    public static class Node<K, V> {
-        public K key;
-        public V value;
-        public Node<K, V> last;
-        public Node<K, V> next;
-
-        public Node(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
-
-    public static class NodeDoubleLinkedList<K, V> {
-        private Node<K, V> head;
-        private Node<K, V> tail;
-
-        public NodeDoubleLinkedList() {
-            this.head = null;
-            this.tail = null;
-        }
-
-        public void addNode(Node<K, V> newNode) {
-            if (newNode == null) {
-                return;
-            }
-
-            if (this.head == null) {
-                this.head = newNode;
-                this.tail = newNode;
-            } else {
-                this.tail.next = newNode;
-                newNode.last = this.tail;
-                this.tail = newNode;
-            }
-        }
-
-        public void moveNodeToTail(Node<K, V> node) {
-            if (this.tail == node) {
-                return;
-            }
-
-            if (this.head == node) {
-                this.head = node.next;
-                this.head.next.last = null;
-            } else {
-                node.last.next = node.next;
-                node.next.last = node.last;
-            }
-
-            node.last = this.tail;
-            node.next = null;
-            this.tail.next = node;
-            this.tail = node;
-        }
-
-        public Node<K, V> removeHead() {
-            if (this.head == null) {
-                return;
-            }
-
-            this.head.next.last = null;
-            this.head.next 
-        }
-
-    
-    
-
-    }
-}
-
-
+//package com.enchanted;
+//
+//
+///**
+// * @ClassName: LRUCache
+// * @Description: LeetCode146.LRU缓存
+// * @PackageName:com.enchanted
+// * @Author Enchanted
+// * @Date 2023/8/18 20:38
+// * @Version 1.0
+// */
+//
+//
+//public class LRUCache {
+//
+//    public static class Node<K, V> {
+//        public K key;
+//        public V value;
+//        public Node<K, V> last;
+//        public Node<K, V> next;
+//
+//        public Node(K key, V value) {
+//            this.key = key;
+//            this.value = value;
+//        }
+//    }
+//
+//    public static class NodeDoubleLinkedList<K, V> {
+//        private Node<K, V> head;
+//        private Node<K, V> tail;
+//
+//        public NodeDoubleLinkedList() {
+//            this.head = null;
+//            this.tail = null;
+//        }
+//
+//        public void addNode(Node<K, V> newNode) {
+//            if (newNode == null) {
+//                return;
+//            }
+//
+//            if (this.head == null) {
+//                this.head = newNode;
+//                this.tail = newNode;
+//            } else {
+//                this.tail.next = newNode;
+//                newNode.last = this.tail;
+//                this.tail = newNode;
+//            }
+//        }
+//
+//        public void moveNodeToTail(Node<K, V> node) {
+//            if (this.tail == node) {
+//                return;
+//            }
+//
+//            if (this.head == node) {
+//                this.head = node.next;
+//                this.head.next.last = null;
+//            } else {
+//                node.last.next = node.next;
+//                node.next.last = node.last;
+//            }
+//
+//            node.last = this.tail;
+//            node.next = null;
+//            this.tail.next = node;
+//            this.tail = node;
+//        }
+//
+//        public Node<K, V> removeHead() {
+//            if (this.head == null) {
+//                return;
+//            }
+//
+//            this.head.next.last = null;
+//            this.head.next
+//        }
+//
+//
+//
+//
+//    }
+//}
+//
+//
